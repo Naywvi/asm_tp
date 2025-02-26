c11933986993e9ac79889a2408781035770ab694
naywvi <nagib.lakhdari.pro@gmail.com>
2025-02-26 12:50:52 +0100

    test

---
+++ Private/HelloWorld/hello.s
@@ -0,0 +1,19 @@
+global _start
+
+section .data
+    msg: db "1337", 0
+
+section .text
+
+_start:
+;sys_write
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 6
+    syscall
+
+_exit:
+    mov rax, 60
+    mov rdi, 0
+    syscall
