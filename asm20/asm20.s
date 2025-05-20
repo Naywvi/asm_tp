ee6cc0232ff767171a70b3baca28a80838cc1176
naywvi <nagib.lakhdari.pro@gmail.com>
2025-05-20 16:19:25 +0200

    asm20

--- asm20/asm20.s
+++ asm20/asm20.s
@@ -1,7 +1,6 @@
 section .data
-    ; Messages
-    listen_msg db 27, "[38;5;221m⏳ Listening on port 4242", 27, "[0m", 10
-    listen_msg_len equ $ - listen_msg
+    listen_msg db 27, "⏳ Listening on port 4242", 27, "[0m", 10
+    listen_len equ $ - listen_msg
     
     prompt db "Type a command: "
     prompt_len equ $ - prompt
@@ -12,352 +11,309 @@ section .data
     goodbye_msg db "Goodbye!", 10
     goodbye_len equ $ - goodbye_msg
     
-    ; Commands
-    ping_cmd db "PING", 0
-    exit_cmd db "EXIT", 0
-    reverse_cmd db "REVERSE ", 0
+    newline db 10
     
-    ; Error messages
-    error_socket db "Error: Cannot create socket", 10
-    error_socket_len equ $ - error_socket
+    ping_cmd db "PING"
+    ping_len equ $ - ping_cmd
     
-    error_bind db "Error: Cannot bind to port", 10
-    error_bind_len equ $ - error_bind
+    exit_cmd db "EXIT"
+    exit_len equ $ - exit_cmd
     
-    error_listen db "Error: Cannot listen", 10
-    error_listen_len equ $ - error_listen
-    
-    ; Socket option
-    socket_option dd 1
+    reverse_cmd db "REVERSE "
+    reverse_len equ $ - reverse_cmd
 
 section .bss
-    server_socket resq 1
-    client_socket resq 1
-    server_addr resb 16
+    buffer resb 1024
+    work_buffer resb 1024
+    
+    server_fd resd 1
+    client_fd resd 1
+    
+    sockaddr_in resb 16
     client_addr resb 16
     addr_len resd 1
-    buffer resb 1024
 
 section .text
 global _start
 
 _start:
     ; Create socket
-    mov rax, 41         ; sys_socket
-    mov rdi, 2          ; AF_INET
-    mov rsi, 1          ; SOCK_STREAM
-    mov rdx, 0          ; Protocol
+    mov eax, 41         ; socket
+    mov edi, 2          ; AF_INET
+    mov esi, 1          ; SOCK_STREAM
+    xor edx, edx        ; IPPROTO_IP
     syscall
     
-    ; Check for error
-    test rax, rax
-    js socket_error
+    test eax, eax
+    js exit_error
     
-    ; Save socket descriptor
-    mov [server_socket], rax
+    mov dword [server_fd], eax
     
-    ; Set SO_REUSEADDR option
-    mov rax, 54         ; sys_setsockopt
-    mov rdi, [server_socket]
-    mov rsi, 1          ; SOL_SOCKET
-    mov rdx, 2          ; SO_REUSEADDR
-    lea r10, [socket_option]
-    mov r8, 4           ; Option length
-    syscall
-    
-    ; Set up server address
-    mov word [server_addr], 2       ; AF_INET
-    mov word [server_addr+2], 0x9a10 ; Port 4242 (network byte order)
-    mov dword [server_addr+4], 0     ; INADDR_ANY
+    ; Prepare sockaddr_in structure
+    mov word [sockaddr_in], 2        ; AF_INET
+    mov word [sockaddr_in+2], 0x9a10 ; Port 4242 (network byte order)
+    mov dword [sockaddr_in+4], 0     ; INADDR_ANY
+    mov dword [sockaddr_in+8], 0     ; Padding
+    mov dword [sockaddr_in+12], 0    ; Padding
     
     ; Bind socket
-    mov rax, 49         ; sys_bind
-    mov rdi, [server_socket]
-    mov rsi, server_addr
-    mov rdx, 16         ; Address length
+    mov eax, 49         ; bind
+    mov edi, dword [server_fd]
+    mov esi, sockaddr_in
+    mov edx, 16         ; sizeof(sockaddr_in)
     syscall
     
-    ; Check for error
-    test rax, rax
-    js bind_error
+    test eax, eax
+    js exit_error
     
-    ; Start listening
-    mov rax, 50         ; sys_listen
-    mov rdi, [server_socket]
-    mov rsi, 5          ; Backlog queue size
+    ; Listen for connections
+    mov eax, 50         ; listen
+    mov edi, dword [server_fd]
+    mov esi, 5          ; backlog
     syscall
     
-    ; Check for error
-    test rax, rax
-    js listen_error
+    test eax, eax
+    js exit_error
     
     ; Print listening message
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; stdout
-    mov rsi, listen_msg
-    mov rdx, listen_msg_len
+    mov eax, 1          ; write
+    mov edi, 1          ; STDOUT
+    mov esi, listen_msg
+    mov edx, listen_len
     syscall
     
     ; Accept connections loop
-    accept_loop:
-        mov dword [addr_len], 16
-        
-        ; Accept a connection
-        mov rax, 43     ; sys_accept
-        mov rdi, [server_socket]
-        mov rsi, client_addr
-        mov rdx, addr_len
-        syscall
-        
-        ; Check for error
-        test rax, rax
-        js accept_loop  ; Try again on error
-        
-        ; Save client socket
-        mov [client_socket], rax
-        
-        ; Fork to handle client
-        mov rax, 57     ; sys_fork
-        syscall
-        
-        ; Check fork result
-        test rax, rax
-        js accept_loop  ; Error, continue accepting
-        jz handle_client ; Child process handles client
-        
-        ; Parent process continues
-        mov rax, 3      ; sys_close
-        mov rdi, [client_socket]
-        syscall
-        
-        jmp accept_loop
-
-; Handle client in child process
+    mov dword [addr_len], 16
+    
+accept_loop:
+    mov eax, 43         ; accept
+    mov edi, dword [server_fd]
+    mov esi, client_addr
+    mov edx, addr_len
+    syscall
+    
+    test eax, eax
+    js accept_loop
+    
+    mov dword [client_fd], eax
+    
+    ; Fork to handle client
+    mov eax, 57         ; fork
+    syscall
+    
+    test eax, eax
+    js close_client
+    jz handle_client    ; Child process
+    
+    ; Parent process
+    mov eax, 3          ; close
+    mov edi, dword [client_fd]
+    syscall
+    
+    jmp accept_loop
+    
+close_client:
+    mov eax, 3          ; close
+    mov edi, dword [client_fd]
+    syscall
+    
+    jmp accept_loop
+    
 handle_client:
-    ; Close server socket in child
-    mov rax, 3          ; sys_close
-    mov rdi, [server_socket]
+    ; Child process handles client
+    mov eax, 3          ; close
+    mov edi, dword [server_fd]
     syscall
     
-    ; Client communication loop
-    client_loop:
-        ; Send prompt
-        mov rax, 1      ; sys_write
-        mov rdi, [client_socket]
-        mov rsi, prompt
-        mov rdx, prompt_len
-        syscall
-        
-        ; Receive command
-        mov rax, 0      ; sys_read
-        mov rdi, [client_socket]
-        mov rsi, buffer
-        mov rdx, 1024
-        syscall
-        
-        ; Check for EOF or error
-        test rax, rax
-        jle client_done
-        
-        ; Null-terminate the command and save length
-        mov rcx, rax
-        dec rcx
-        mov byte [buffer + rcx], 0
-        
-        ; Check for PING command
-        mov rdi, buffer
-        mov rsi, ping_cmd
-        call compare_strings
-        cmp rax, 1
-        je handle_ping
-        
-        ; Check for EXIT command
-        mov rdi, buffer
-        mov rsi, exit_cmd
-        call compare_strings
-        cmp rax, 1
-        je handle_exit
-        
-        ; Check for REVERSE command
-        mov rdi, buffer
-        mov rsi, reverse_cmd
-        call starts_with
-        cmp rax, 1
-        je handle_reverse
-        
-        ; Unknown command, loop back for another
-        jmp client_loop
-        
-    ; PING command handler
-    handle_ping:
-        mov rax, 1      ; sys_write
-        mov rdi, [client_socket]
-        mov rsi, pong_msg
-        mov rdx, pong_len
-        syscall
-        jmp client_loop
-        
-    ; EXIT command handler
-    handle_exit:
-        mov rax, 1      ; sys_write
-        mov rdi, [client_socket]
-        mov rsi, goodbye_msg
-        mov rdx, goodbye_len
-        syscall
-        ; Fall through to client_done
-        
-    ; Client disconnection handler
-    client_done:
-        mov rax, 3      ; sys_close
-        mov rdi, [client_socket]
-        syscall
-        
-        mov rax, 60     ; sys_exit
-        xor rdi, rdi    ; Return code 0
-        syscall
-        
-    ; REVERSE command handler
-    handle_reverse:
-        add rdi, 8      ; Skip "REVERSE "
-        call reverse_string
-        
-        mov rax, 1      ; sys_write
-        mov rdi, [client_socket]
-        mov rsi, buffer
-        mov rdx, rcx
-        syscall
-        jmp client_loop
-
-; Error handlers
-socket_error:
-    mov rax, 1          ; sys_write
-    mov rdi, 2          ; stderr
-    mov rsi, error_socket
-    mov rdx, error_socket_len
+client_loop:
+    ; Send prompt
+    mov eax, 1          ; write
+    mov edi, dword [client_fd]
+    mov esi, prompt
+    mov edx, prompt_len
     syscall
     
-    mov rax, 60         ; sys_exit
-    mov rdi, 1          ; Error code
+    ; Receive command
+    mov eax, 0          ; read
+    mov edi, dword [client_fd]
+    mov esi, buffer
+    mov edx, 1024
     syscall
     
-bind_error:
-    mov rax, 1          ; sys_write
-    mov rdi, 2          ; stderr
-    mov rsi, error_bind
-    mov rdx, error_bind_len
+    test eax, eax
+    jle client_done
+    
+    mov ecx, eax
+    mov byte [buffer + eax - 1], 0   ; Strip newline
+    
+    ; Check for PING command
+    mov esi, buffer
+    mov edi, ping_cmd
+    mov edx, ping_len
+    call str_compare
+    test eax, eax
+    jnz handle_ping
+    
+    ; Check for EXIT command
+    mov esi, buffer
+    mov edi, exit_cmd
+    mov edx, exit_len
+    call str_compare
+    test eax, eax
+    jnz handle_exit
+    
+    ; Check for REVERSE command
+    mov esi, buffer
+    mov edi, reverse_cmd
+    mov edx, reverse_len
+    call str_startswith
+    test eax, eax
+    jnz handle_reverse
+    
+    jmp client_loop
+    
+handle_ping:
+    mov eax, 1          ; write
+    mov edi, dword [client_fd]
+    mov esi, pong_msg
+    mov edx, pong_len
     syscall
     
-    mov rax, 3          ; sys_close
-    mov rdi, [server_socket]
+    jmp client_loop
+    
+handle_exit:
+    mov eax, 1          ; write
+    mov edi, dword [client_fd]
+    mov esi, goodbye_msg
+    mov edx, goodbye_len
     syscall
     
-    mov rax, 60         ; sys_exit
-    mov rdi, 1          ; Error code
+client_done:
+    mov eax, 3          ; close
+    mov edi, dword [client_fd]
     syscall
     
-listen_error:
-    mov rax, 1          ; sys_write
-    mov rdi, 2          ; stderr
-    mov rsi, error_listen
-    mov rdx, error_listen_len
+    mov eax, 60         ; exit
+    xor edi, edi        ; Exit code 0
     syscall
     
-    mov rax, 3          ; sys_close
-    mov rdi, [server_socket]
+handle_reverse:
+    ; Copy text after "REVERSE " to work buffer
+    mov esi, buffer
+    add esi, reverse_len
+    mov edi, work_buffer
+    
+copy_loop:
+    mov al, [esi]
+    mov [edi], al
+    test al, al
+    jz copy_done
+    
+    inc esi
+    inc edi
+    jmp copy_loop
+    
+copy_done:
+    ; Get length of string to reverse
+    mov esi, work_buffer
+    call strlen
+    
+    ; Reverse the string
+    mov ecx, eax
+    mov esi, work_buffer
+    mov edi, buffer
+    call reverse
+    
+    ; Add newline
+    mov byte [buffer + eax], 10
+    inc eax
+    
+    ; Send reversed string
+    mov edx, eax
+    mov eax, 1          ; write
+    mov edi, dword [client_fd]
+    mov esi, buffer
     syscall
     
-    mov rax, 60         ; sys_exit
-    mov rdi, 1          ; Error code
+    jmp client_loop
+    
+exit_error:
+    mov eax, 60         ; exit
+    mov edi, 1          ; Exit code 1
     syscall
-
-; Helper functions
-; Compare two strings (rdi = string1, rsi = string2)
-; Returns 1 if equal, 0 if not
-compare_strings:
-    xor rcx, rcx
-compare_loop:
-    mov al, [rdi + rcx]
-    mov dl, [rsi + rcx]
     
-    cmp al, 0
-    je compare_end      ; If we hit null in string1
+; String functions
+; Compare two strings up to length
+; esi = str1, edi = str2, edx = length
+; Returns eax=1 if equal, eax=0 if not
+str_compare:
+    xor ecx, ecx
     
-    cmp dl, 0
-    je compare_not_equal ; If we hit null in string2 but not string1
+str_cmp_loop:
+    cmp ecx, edx
+    jge str_cmp_equal
     
-    cmp al, dl
-    jne compare_not_equal ; Characters don't match
+    mov al, [esi + ecx]
+    cmp al, [edi + ecx]
+    jne str_cmp_not_equal
     
-    inc rcx
-    jmp compare_loop
+    inc ecx
+    jmp str_cmp_loop
     
-compare_end:
-    cmp dl, 0           ; Both null? Then equal
-    jne compare_not_equal
-    
-    mov rax, 1          ; Strings match
+str_cmp_equal:
+    mov eax, 1
     ret
     
-compare_not_equal:
-    xor rax, rax        ; Strings don't match
-    ret
-
-; Check if string starts with prefix (rdi = string, rsi = prefix)
-; Returns 1 if string starts with prefix, 0 if not
-starts_with:
-    xor rcx, rcx
-starts_with_loop:
-    mov al, [rsi + rcx] ; Get prefix character
-    cmp al, 0
-    je starts_with_yes  ; End of prefix - we matched all of it
-    
-    mov dl, [rdi + rcx] ; Get string character
-    cmp dl, 0
-    je starts_with_no   ; End of string before end of prefix
-    
-    cmp al, dl
-    jne starts_with_no  ; Characters don't match
-    
-    inc rcx
-    jmp starts_with_loop
-    
-starts_with_yes:
-    mov rax, 1
+str_cmp_not_equal:
+    xor eax, eax
     ret
     
-starts_with_no:
-    xor rax, rax
+; Check if string starts with prefix
+; esi = string, edi = prefix, edx = prefix length
+; Returns eax=1 if starts with, eax=0 if not
+str_startswith:
+    call str_compare
     ret
-
-; Reverse a string (rdi = string to reverse)
-; Result stored in buffer, length in rcx
-reverse_string:
-    xor rcx, rcx
     
-    ; Find length of string
-length_loop:
-    cmp byte [rdi + rcx], 0
-    je do_reverse
-    inc rcx
-    jmp length_loop
+; Get length of null-terminated string
+; esi = string
+; Returns eax = length
+strlen:
+    xor eax, eax
     
-do_reverse:
-    mov rdx, rcx        ; Save length
-    xor r8, r8          ; Output position
+strlen_loop:
+    cmp byte [esi + eax], 0
+    je strlen_done
     
-    ; Add characters in reverse
+    inc eax
+    jmp strlen_loop
+    
+strlen_done:
+    ret
+    
+; Reverse a string
+; esi = source string, edi = destination buffer, ecx = length
+; Returns eax = length of reversed string
+reverse:
+    mov eax, ecx
+    dec ecx
+    xor edx, edx
+    
 reverse_loop:
-    cmp rdx, 0
-    je add_newline
+    cmp edx, eax
+    jge reverse_done
     
-    dec rdx
-    mov al, [rdi + rdx]
-    mov [buffer + r8], al
-    inc r8
+    mov bl, [esi + ecx]
+    mov [edi + edx], bl
+    
+    inc edx
+    dec ecx
+    
+    cmp ecx, 0
+    jl reverse_done
     jmp reverse_loop
     
-add_newline:
-    mov byte [buffer + r8], 10 ; Add newline
-    inc r8
-    mov rcx, r8         ; Save final length
+reverse_done:
+    mov byte [edi + edx], 0
+    mov eax, edx
     ret
