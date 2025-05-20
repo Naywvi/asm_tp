2c91e865eb0d27a79b0870ce1e983c67a11e2594
naywvi <nagib.lakhdari.pro@gmail.com>
2025-05-20 15:52:15 +0200

    clean the bot

--- asm19/asm19.s
+++ asm19/asm19.s
@@ -1,32 +1,11 @@
 section .data
-    port1 db "1337", 0
-    port2 db "4242", 0
-    netstat_cmd db "/bin/netstat", 0
-    grep_cmd db "/bin/grep", 0
-    awk_cmd db "/usr/bin/awk", 0
-    xargs_cmd db "/usr/bin/xargs", 0
-    kill_cmd db "/bin/kill", 0
-    
-    arg_n db "-n", 0
-    arg_p db "-p", 0
-    arg_u db "-u", 0
-    arg_t db "-t", 0
-    arg_a db "-a", 0
-    
-    grep_port1 db "--", 0
-    grep_port2 db "--", 0
-    
-    awk_arg db "{print $7}", 0
-    xargs_arg db "-9", 0
-    
     sh_cmd db "/bin/sh", 0
     sh_arg db "-c", 0
-    command db "netstat -tunap | grep -E ':1337|:4242' | awk '{print $7}' | cut -d/ -f1 | grep -v '-' | sort -u | xargs -r kill -9", 0
+    
+    command db "lsof -i :1337,4242 | grep -v PID | awk '{print $2}' | xargs kill -9 2>/dev/null || true", 0
     
     success_msg db "Killed processes listening on ports 1337 and 4242", 10
     success_len equ $ - success_msg
-    error_msg db "Error executing shell command", 10
-    error_len equ $ - error_msg
 
 section .text
     global _start
@@ -66,22 +45,6 @@ parent:
     mov rax, 61         ; sys_wait4
     syscall
     
-    ; Check if the child exited successfully
-    test rdx, rdx
-    jz success
-    
-    ; Child failed, display error message
-    mov rax, 1          ; sys_write
-    mov rdi, 2          ; stderr
-    mov rsi, error_msg
-    mov rdx, error_len
-    syscall
-    
-    mov rax, 60         ; sys_exit
-    mov rdi, 1          ; Exit code 1
-    syscall
-    
-success:
     ; Display success message
     mov rax, 1          ; sys_write
     mov rdi, 1          ; stdout
